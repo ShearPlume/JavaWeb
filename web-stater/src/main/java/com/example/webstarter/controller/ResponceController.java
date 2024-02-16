39ad77900a5559fac42d3e39526db956803af3d4
ShearPlume <1830967718@qq.com>
2024-02-16 20:10:08 +0800

    Javaweb项目学习 请求响应：统一返回Result对象 添加pom文件

--- web-stater/src/main/java/com/example/webstarter/controller/ResponceController.java
+++ web-stater/src/main/java/com/example/webstarter/controller/ResponceController.java
@@ -1,6 +1,7 @@
 package com.example.webstarter.controller;
 
 import com.example.webstarter.pojo.Address;
+import com.example.webstarter.pojo.Result;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,22 +11,52 @@ import java.util.List;
 @RestController
 public class ResponceController {
 
-    @RequestMapping("/hello")
-    public String hello() {
-        System.out.println("Hello World");
-        return "hello world";
-    }
+//    @RequestMapping("/hello")
+//    public String hello() {
+//        System.out.println("Hello World");
+//        return "hello world";
+//    }
+//
+//    @RequestMapping("/getAddr")
+//    public Address getAddr() {
+//        Address add = new Address();
+//        add.setCity("Chengdu");
+//        add.setProv("SiChuan");
+//        return add;
+//    }
+//
+//    @RequestMapping("/listAddr")
+//    public List<Address> listAddr() {
+//        List<Address> addrs = new ArrayList<Address>();
+//        Address add1 = new Address();
+//        add1.setCity("Chengdu");
+//        add1.setProv("SiChuan");
+//        Address add2 = new Address();
+//        add2.setCity("Guangzhou");
+//        add2.setProv("GuangDong");
+//        addrs.add(add1);
+//        addrs.add(add2);
+//        return addrs;
+//    }
+    //返回result：
+@RequestMapping("/hello")
+
+public Result hello() {
+    System.out.println("Hello World");
+    String data="hello world";
+    return Result.success(data);
+}
 
     @RequestMapping("/getAddr")
-    public Address getAddr() {
+    public Result getAddr() {
         Address add = new Address();
         add.setCity("Chengdu");
         add.setProv("SiChuan");
-        return add;
+        return Result.success(add);
     }
 
     @RequestMapping("/listAddr")
-    public List<Address> listAddr() {
+    public Result listAddr() {
         List<Address> addrs = new ArrayList<Address>();
         Address add1 = new Address();
         add1.setCity("Chengdu");
@@ -35,6 +66,6 @@ public class ResponceController {
         add2.setProv("GuangDong");
         addrs.add(add1);
         addrs.add(add2);
-        return addrs;
+        return Result.success(addrs);
     }
 }
